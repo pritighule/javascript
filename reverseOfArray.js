9367f09c91410a50317f27306958cfcb8b35b088
pritighule <pritiwarungse21@gmail.com>
2023-09-20 19:53:32 +0530

    001: updated file with logic for reverse of an array

--- reverseOfArray.js
+++ reverseOfArray.js
@@ -1,1 +1,14 @@
-abc
+const arr = [1,2,3];
+
+function reverseArray(arr)
+{
+    let arr1= [];
+    for(let i= arr.length - 1; i >=0; i--)
+    {
+        arr1.push(arr[i]);
+    }
+
+    return arr1;
+}
+
+console.log(reverseArray(arr));
